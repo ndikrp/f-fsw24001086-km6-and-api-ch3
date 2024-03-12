79dd9cbcb1bfa40751083062871cfdcc5ae0eda6
ndikrp <andhikarizky36@gmail.com>
2024-03-12 19:06:15 +0700

    added endpoint POST /cars

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const cars = require('./cars')
+const { v4: uuidv4 } = require('uuid');
+const app = express()
+
 
 // Get list of cars
 router.get('/cars', (req, res) => {
@@ -16,6 +19,31 @@ router.get('/cars/:id', (req, res) => {
     const indentedJson = JSON.stringify(car, null, 2)
     res.setHeader('Content-Type', 'application/json')
     res.send(indentedJson)
+})
+
+router.post('/cars', (req, res) => {
+    const newCarData = req.body
+    const requireFields = [
+        'plate', 'manufacture', 'model', 'image',
+        'rentPerDay', 'capacity', 'description', 'availableAt',
+        'transmission', 'available', 'type', 'year', 'options', 'specs'
+    ]
+    for (const field of requireFields) {
+        if (!newCarData[field]) {
+            return res.status(400).json({ error: `Missing required field ${field}` })
+        } else if (cars.some(car => car.plate === newCarData.plate)) {
+            return res.status(400).json({ error: 'Plate number cannot be the same' })
+        }
+    }
+
+    const newCarId = uuidv4()
+    const newCar = {
+        id: newCarId,
+        ...newCarData
+    }
+    cars.push(newCar);
+    res.status(201).json({ Status: 'Success', message: 'New car added!', newCar });
+
 })
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const { PORT = 8000 } = process.env
 const routes = require('./routes')
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({ message: 'Ping Succesfuly!' })
 })
 
 app.use(routes)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT} `)
